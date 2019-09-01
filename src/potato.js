f7bd71aa99d7edc7b8e6add9366e9652cdbe9495
Jay <psatdoctor@naver.com>
2019-09-01 19:46:12 +0900

    Prop types

--- src/potato.js
+++ src/potato.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-
-function Potato({name, picture}){
+function Potato({name, picture, rating}){
     return (
         <div>
             <h2>I love {name}</h2>
-            <img src={picture}/>
+            <h4>{rating}/5.0</h4>
+            <img src={picture} alt={name}/>
+
         </div>
     );
 
+
+
 }
+
+Potato.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    rating: PropTypes.number
+}
+
 
 
 export default Potato;
